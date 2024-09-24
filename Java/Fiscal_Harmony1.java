fd3dc28c39f5776e5166e8700149f0a3a9305d15
Ram <140935795+tcker@users.noreply.github.com>
2024-09-24 12:38:43 +0800

    Update Fiscal_Harmony1.java

--- Java/Fiscal_Harmony1.java
+++ Java/Fiscal_Harmony1.java
@@ -1,11 +1,42 @@
 import java.util.*;
 
 class Fiscal {
-  public class static void main(String[] args) {
-  Scanner scanner = Scanner(System.in);
+  public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);
 
+
+System.out.println("=============================================================");
+System.out.println("\t\tWelcome to Fiscal Harmony");
+System.out.println("=============================================================");
+System.out.println("Planning your future for a financially free life.");
+System.out.println("[1]. Sign In [2]. Sign Up");
+System.out.print("> ");
+int UserLoginOption = scanner.nextInt();
     
-  }
+    
+    switch (UserLoginOption) {
+        case 1: 
+            System.out.println("Enter your Credentials");
+            System.out.print("Username: ");
+            String Username = scanner.nextLine();
+            
+            System.out.print("Password: ");
+            String Password = scanner.nextLine();
+            
+            System.out.println("Welcome! " + Username);
+        case 2:
+            System.out.println("Register");
+            break;
+    }
+    
 
-  
+
+
+
+
+
+
+
+
+  }
 }
